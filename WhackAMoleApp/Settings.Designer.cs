d3144a7a1f9aec0f3f4ce50b455c7e5425e1810d
Richard Shaw <richard.shaw@theactigraph.com>
2017-06-12 00:09:33 -0500

    Added in about tab, added in credits for music/sounds. Removed Scores tab, moved clear scores button to general area. There is something strange happening to the music after adjusting the volume after a while...

--- WhackAMoleApp/Settings.Designer.cs
+++ WhackAMoleApp/Settings.Designer.cs
@@ -28,22 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.tabGeneralSettings = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbVolume = new System.Windows.Forms.TrackBar();
             this.txtPlayerName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.cmbDifficulty = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tabControl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabAbout = new System.Windows.Forms.TabPage();
             this.btnClearScores = new System.Windows.Forms.Button();
-            this.tbVolume = new System.Windows.Forms.TrackBar();
-            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabGeneralSettings.SuspendLayout();
-            this.tabControl.SuspendLayout();
-            this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).BeginInit();
+            this.tabControl.SuspendLayout();
+            this.tabAbout.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCancel
@@ -51,7 +53,7 @@
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.AutoSize = true;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(236, 191);
+            this.btnCancel.Location = new System.Drawing.Point(273, 258);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 1;
@@ -62,7 +64,7 @@
             // 
             this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSave.AutoSize = true;
-            this.btnSave.Location = new System.Drawing.Point(155, 191);
+            this.btnSave.Location = new System.Drawing.Point(192, 258);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 2;
@@ -71,6 +73,7 @@
             // 
             // tabGeneralSettings
             // 
+            this.tabGeneralSettings.Controls.Add(this.btnClearScores);
             this.tabGeneralSettings.Controls.Add(this.label3);
             this.tabGeneralSettings.Controls.Add(this.tbVolume);
             this.tabGeneralSettings.Controls.Add(this.txtPlayerName);
@@ -80,15 +83,36 @@
             this.tabGeneralSettings.Location = new System.Drawing.Point(4, 22);
             this.tabGeneralSettings.Name = "tabGeneralSettings";
             this.tabGeneralSettings.Padding = new System.Windows.Forms.Padding(3);
-            this.tabGeneralSettings.Size = new System.Drawing.Size(291, 147);
+            this.tabGeneralSettings.Size = new System.Drawing.Size(328, 214);
             this.tabGeneralSettings.TabIndex = 0;
             this.tabGeneralSettings.Text = "General";
             this.tabGeneralSettings.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Volume";
+            // 
+            // tbVolume
+            // 
+            this.tbVolume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbVolume.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbVolume.Location = new System.Drawing.Point(176, 78);
+            this.tbVolume.Maximum = 100;
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(146, 45);
+            this.tbVolume.TabIndex = 4;
+            this.tbVolume.TickFrequency = 10;
+            this.tbVolume.Value = 50;
+            // 
             // txtPlayerName
             // 
             this.txtPlayerName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPlayerName.Location = new System.Drawing.Point(139, 51);
+            this.txtPlayerName.Location = new System.Drawing.Point(176, 51);
             this.txtPlayerName.Name = "txtPlayerName";
             this.txtPlayerName.Size = new System.Drawing.Size(146, 20);
             this.txtPlayerName.TabIndex = 3;
@@ -107,7 +131,7 @@
             this.cmbDifficulty.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbDifficulty.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbDifficulty.FormattingEnabled = true;
-            this.cmbDifficulty.Location = new System.Drawing.Point(139, 23);
+            this.cmbDifficulty.Location = new System.Drawing.Point(176, 23);
             this.cmbDifficulty.Name = "cmbDifficulty";
             this.cmbDifficulty.Size = new System.Drawing.Size(146, 21);
             this.cmbDifficulty.TabIndex = 1;
@@ -127,52 +151,48 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl.Controls.Add(this.tabGeneralSettings);
-            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabAbout);
             this.tabControl.Location = new System.Drawing.Point(12, 12);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(299, 173);
+            this.tabControl.Size = new System.Drawing.Size(336, 240);
             this.tabControl.TabIndex = 3;
             // 
-            // tabPage1
+            // tabAbout
             // 
-            this.tabPage1.Controls.Add(this.btnClearScores);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(291, 92);
-            this.tabPage1.TabIndex = 1;
-            this.tabPage1.Text = "Scores";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabAbout.Controls.Add(this.textBox1);
+            this.tabAbout.Location = new System.Drawing.Point(4, 22);
+            this.tabAbout.Name = "tabAbout";
+            this.tabAbout.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAbout.Size = new System.Drawing.Size(328, 214);
+            this.tabAbout.TabIndex = 2;
+            this.tabAbout.Text = "About";
+            this.tabAbout.UseVisualStyleBackColor = true;
             // 
             // btnClearScores
             // 
-            this.btnClearScores.Location = new System.Drawing.Point(85, 34);
+            this.btnClearScores.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClearScores.Location = new System.Drawing.Point(9, 185);
             this.btnClearScores.Name = "btnClearScores";
-            this.btnClearScores.Size = new System.Drawing.Size(114, 23);
-            this.btnClearScores.TabIndex = 0;
+            this.btnClearScores.Size = new System.Drawing.Size(313, 23);
+            this.btnClearScores.TabIndex = 4;
             this.btnClearScores.Text = "Clear High Scores";
             this.btnClearScores.UseVisualStyleBackColor = true;
             // 
-            // tbVolume
+            // textBox1
             // 
-            this.tbVolume.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.tbVolume.Location = new System.Drawing.Point(139, 78);
-            this.tbVolume.Maximum = 100;
-            this.tbVolume.Name = "tbVolume";
-            this.tbVolume.Size = new System.Drawing.Size(146, 45);
-            this.tbVolume.TabIndex = 4;
-            this.tbVolume.TickFrequency = 10;
-            this.tbVolume.Value = 50;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 78);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Volume";
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(322, 208);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
             // Settings
             // 
@@ -180,7 +200,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(323, 226);
+            this.ClientSize = new System.Drawing.Size(360, 293);
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnCancel);
@@ -191,9 +211,10 @@
             this.Text = "Settings";
             this.tabGeneralSettings.ResumeLayout(false);
             this.tabGeneralSettings.PerformLayout();
-            this.tabControl.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).EndInit();
+            this.tabControl.ResumeLayout(false);
+            this.tabAbout.ResumeLayout(false);
+            this.tabAbout.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,9 +230,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtPlayerName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Button btnClearScores;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TrackBar tbVolume;
+        private System.Windows.Forms.Button btnClearScores;
+        private System.Windows.Forms.TabPage tabAbout;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
